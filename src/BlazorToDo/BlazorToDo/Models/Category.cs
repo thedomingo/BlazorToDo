b1daed9c872cd184dfbd2c6ea3d917df51a7e3fd
Domen <domen.hrastnik@gmail.com>
2020-01-07 21:21:50 +0100

    dodajanje kategorij in taskov

---
+++ src/BlazorToDo/BlazorToDo/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorToDo.Models
+{
+    public class Category
+    {
+        public string Name { get; set; }
+
+        public List<TodoItem> TodoItems { get; set; }
+
+        public Category()
+        {
+            TodoItems = new List<TodoItem>();
+        }
+    }
+}
--- src/BlazorToDo/BlazorToDo/Startup.cs
+++ src/BlazorToDo/BlazorToDo/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using BlazorToDo.Data;
+using Blazored.Modal;
 
 namespace BlazorToDo
 {
@@ -29,6 +30,7 @@ namespace BlazorToDo
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+            services.AddBlazoredModal();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
